a1ace6f32c64dfab65c3aedf86ff20625668a86f
farman-ali72 <codingwithfa.72@gmail.com>
2025-04-19 01:55:31 +0500

    chapter 01 to 05 added

---
+++ app.js
@@ -0,0 +1,161 @@
+// Date: 15-03-25
+// javascript class 01
+// chapter cover 01 to 03
+
+// CHAPTER 01 ===START===
+// Chapter 1 (Alerts)
+
+// var firstName = "Farman";
+// var lastName = "Ali";
+// var email = "Fa.72@gmail.com";
+// var phoneNumber = "1234567890";
+// var password = "mySecret123";
+
+// // Alerts
+// alert(firstName);
+// alert(lastName);
+// alert(email);
+// alert(phoneNumber);
+// alert(password);
+
+
+// alert("I'm learning JavaScript!");
+// ====End====
+
+// CHAPTER 02 ===START===
+// Chapter 2 (Variables for string)
+
+// var fullName = "farman ali";
+// alert(fullName);
+
+// var MyFav;
+// MyFav = "black";
+// alert(MyFav);
+
+// var teamName = "Code Warriors";
+// alert(teamName);
+
+// var bestMan = "Farman";
+// bestMan = "Ali";     
+// alert(bestMan);          
+// ====End====
+
+// CHAPTER 03 ===START===
+// Chapter 3 (Variables for numbers)
+
+// var caseQty;
+// caseQty = 804;
+// alert(caseQty)
+
+// var num = "8";
+// num = Number(num);
+// var result = num + 5;
+// alert(result)
+
+// var sum;
+// sum = 2 + 2;
+// alert(sum)
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal)
+
+// var num = 10;
+// num = num + 20;
+// alert(num);
+
+// =================================xxx=================================
+
+// Date: 12-04-25
+// javascript class 02
+// chapter cover 04 to 11
+
+// chapter 04
+// (Variable names Legal and Illegal)
+
+// var productCost = 3.45;
+// console.log(productCost);
+
+// var age = 23;
+// console.log(age)
+
+// var firstName ="Farman"
+// var lastName = " Ali"
+// var fullName = firstName + lastName;
+// console.log(fullName);
+
+// Legal & Illegal Variables
+// Legal Variable Names
+// var name;
+// var _name;
+// var $name;
+// var name1;
+// var firstName;
+// var first_name;
+// var user123;
+// var myVar;
+// var $dollar_and_underscore_;
+// var camelCaseVariable;
+
+// Illegal Variable Names
+// var 1name;         // Starts with number
+// var first name;    // Contains space
+// var var;           // Reserved keyword
+// var function;      // Reserved keyword
+// var @name;         // Invalid character "@"
+// var my-name;       // Hyphen not allowed
+// var new;           // Reserved keyword
+// var let;           
+// ==========xxxxxxxxxx==========
+
+// Chapter 5 
+// (Math Expression I)
+
+// var remainder = 13 % 3;
+// console.log(remainder); //Output: 1
+
+// var num = 20 % 6;
+// console.log(num); //Output: 2
+
+// var largeNum = 1000 * 2000;
+// console.log(largeNum) //output: 2000000
+
+// var x = 20;
+// var y = 10;
+// var result = x - y;
+// console.log(result); //output; 10
+
+// var remainderNumber = 15 % 4;
+// console.log(remainderNumber); //output: 03
+
+// var num = 10 * 5;
+// alert(num)
+// ==========xxxxxxxxxx==========
+
+// Chapter 6 
+// (Math Expression II)
+
+// var x = 5;
+// x += 1; // now x is 6
+// let x = 5;
+// x++; // now x is 6
+
+// var x = 100;
+// x--;          // x becomes 99
+// console.log(x); // Output: 99
+
+var x = 50;
+var y = x++;
+console.log(y)
+
+
+
+
+
+
+
+
+
+
+
